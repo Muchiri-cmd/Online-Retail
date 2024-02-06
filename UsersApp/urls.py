17f697989d2004d880c96a6f7c1f7e739cb2deac
Muchiri-cmd <davismuchiri21@gmail.com>
2024-02-06 22:23:09 +0300

    Add login feature

--- UsersApp/urls.py
+++ UsersApp/urls.py
@@ -3,5 +3,6 @@ from.views import *
 
 app_name="UsersApp"
 urlpatterns = [
-    path("sign-up/",sign_up_view,name="signup")
+    path("sign-up/",sign_up_view,name="signup"),
+    path("sign-in",sign_in_view,name="signin"),
 ]
--- UsersApp/views.py
+++ UsersApp/views.py
@@ -16,6 +16,7 @@ def sign_up_view(request):
             messages.success(request,f"Hello {username} you signed up successfully")
             new_user=authenticate(username=form.cleaned_data['email'],password=form.cleaned_data['password1'])
             print("Succesful signup")
+            #login user to current sessions and redirect to home page
             login(request,new_user)
             return redirect("main:index")
     else:
@@ -25,3 +26,27 @@ def sign_up_view(request):
             'form':form,
     }
     return render(request,"UsersApp/sign-up.html",context)
+
+def sign_in_view(request):
+    if request.user.is_authenticated:
+        return redirect("main:index")
+    
+    if request.method == "POST": 
+        email = request.POST['email']
+        print(email)
+        password = request.POST['password']
+
+        try:
+            # Check if user exists
+            user = User.objects.get(email=email)
+            if user is not None:
+                print("User account present")
+                login(request, user)
+                messages.success(request, "You're successfully logged in")
+                return redirect("main:index")
+            else:
+                print("Not registered")
+        except:
+            messages.warning(request, "Something went wrong. Please try again.")
+
+    return render(request, "UsersApp/sign-in.html")
